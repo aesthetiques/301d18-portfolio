82e6d4221c32174ac6427916d12968d78a4653d9
Caleb <caleb.wells1@gmail.com>
2017-03-15 01:58:56 -0700

    wat

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -2,13 +2,13 @@
 
 $(function(){
   //config of slide distance, and animation speed
-  var slideWidth = 720;
-  var slideAnimationSpeed = 1000;
-  var pause = 5000;
+  var slideWidth = 1215;
+  var slideAnimationSpeed = 2000;
+  var pause = 2000;
   var currentSlide = 1;
 
   //grab DOM elements
-  var $slideshowArticle = $('.slideshow-article'); //<article class="slideshow-article divs>"
+  var $slideshowArticle = $('#slideshow-article'); //<article class="slideshow-article divs>"
   var $slideshowContainer = $slideshowArticle.find('.slideshow');
   var $mySlides = $slideshowContainer.find('.my-slides');
 
@@ -16,7 +16,7 @@ $(function(){
   setInterval(function(){
     $slideshowContainer.animate({'margin-left': '-=' + slideWidth}, 1000, function(){
       currentSlide ++;
-      if(currentSlide === $slides.length){
+      if(currentSlide === $mySlides.length){
         currentSlide = 1;
         $slideshowContainer.css('margin-left', 0);
       }
