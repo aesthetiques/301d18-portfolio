31fd7a9c7a0209b9e92451369a3b49375b62600c
Caleb <caleb.wells1@gmail.com>
2017-03-15 01:16:32 -0700

    finished main layout wireframes, working on slideshow now

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,1 +1,33 @@
 'use script';
+
+$(function(){
+  //config of slide distance, and animation speed
+  var slideWidth = 720;
+  var slideAnimationSpeed = 1000;
+  var pause = 5000;
+  var currentSlide = 1;
+
+  //grab DOM elements
+  var $slideshowArticle = $('.slideshow-article'); //<article class="slideshow-article divs>"
+  var $slideshowContainer = $slideshowArticle.find('.slideshow');
+  var $mySlides = $slideshowContainer.find('.my-slides');
+
+  //animate slideshow on 5s interval
+  setInterval(function(){
+    $slideshowContainer.animate({'margin-left': '-=' + slideWidth}, 1000, function(){
+      currentSlide ++;
+      if(currentSlide === $slides.length){
+        currentSlide = 1;
+        $slideshowContainer.css('margin-left', 0);
+      }
+    });
+  }, pause);
+  // intend this to be for the clicks on the arrows.
+  // $mySlides.click(function(){
+  //   $slideshowContainer.animate({'margin-left': '-=' + slideWidth}, 1000, function(){
+  //     if(currentSlide){
+  //       asd;
+  //     }
+  //   });
+  // });
+});
