af25ebb873ce1d0c7ab3b672a44d85be7f623b68
Caleb <caleb.wells1@gmail.com>
2017-03-23 13:57:48 -0700

    functional

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 //Initialize your project using NPM to create and populate a package.json file
 const express = require('express');
-// : Require the Express package that you installed via NPM, and instantiate the app
-// Remember to install express, and be sure that it's been added to your package.json as a dependency
+//Require the Express package that you installed via NPM, and instantiate the app
+//Remember to install express, and be sure that it's been added to your package.json as a dependency
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -14,10 +14,16 @@ app.use(express.static('./public'));
 //Using the response object, send the index.html file back to the user
 app.get('/index', function(request, response) {
   response.sendFile('public/index.html', {root: '.'});
-
 });
 
+//Log to the console a message that lets you know which port your server has started on
 app.listen(PORT, function() {
   console.log('you are on port number: ' + PORT + ', sucka.');
-  //Log to the console a message that lets you know which port your server has started on
+});
+
+//Write a new route that will handle a request and send the new.html file back to the user
+//to be used later on
+app.get('/new', function(request, response){
+  console.log('Make a new page');
+  response.sendFile('public/new.html', {root: '.'});
 });
